cf2adcdcb5206f716e10c0b7a07f9c50d795c1f7
ayoubsofi <ayoub@email.com>
2026-07-24 16:21:55 +0100

    pass ticket variable to reservation success view

---
+++ app/Models/Ticket.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Ticket extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'reservation_id',
+        'user_id',
+        'ticket_number',
+    ];
+
+    public function reservation(): BelongsTo
+    {
+        return $this->belongsTo(Reservation::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
